06c787c763bfa5fdd445ed071a2cad91d5a4cdd0
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2025-08-31 01:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (64.43%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -10,24 +10,25 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-       if(head == null || k==0) return head;
-        ListNode tail = head;
-        int len = 1;
-        while(tail.next != null){
-            tail = tail.next;
-            len++;
-        }
-        k= k% len;
-        if (k == 0) return head;
-        tail.next = head;
-         ListNode newLastNode = head;
-         for (int i =1; i< len - k; i++){
-            newLastNode = newLastNode.next;
-         }
-         head = newLastNode.next;
-         newLastNode.next = null;
+       if(head==null || k==0)
+       return head;
+       ListNode tail = head;
+       int len = 1;
+       while(tail.next != null){
+        tail = tail.next;
+        len++;
+       }
+       k = k%len;
+       if (k== 0) return head;
+       tail.next = head;
+       ListNode newLastNode = head;
+       for(int i =1;i<len-k;i++){
+        newLastNode = newLastNode.next;
 
-         return head;
+       }
+       head = newLastNode.next;
+       newLastNode.next = null;
+       return head;
     }
         
     }
