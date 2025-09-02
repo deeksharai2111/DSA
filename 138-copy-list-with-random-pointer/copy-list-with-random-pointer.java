da1b775c72387079af19358f4951b21595c9bff3
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2025-09-03 01:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (70.05%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -12,36 +12,36 @@ class Node {
     }
 }
 */
-
-class Solution {
-    public Node copyRandomList(Node head) {
-      if (head == null) return null;
-      Node temp = head;
-      while(temp != null){
-        Node newNode = new Node(temp.val);
-        newNode.next = temp.next;
-        temp.next = newNode;
+class Solution{
+    public Node copyRandomList(Node head){
+     if(head==null)return null;
+     Node temp = head;
+   
+     while(temp!=null){
+          Node newNode = new Node(temp.val);
+        newNode.next= temp.next;
+        temp.next=newNode;
         temp = newNode.next;
-      }  
-      temp = head;
-      while (temp != null){
-        if (temp.random != null){
-        temp.next.random = temp.random.next;
-      }
-        temp = temp.next.next;
-      }
-      temp = head;
-      Node newHead = head.next;
-      Node newtemp = newHead;
-      while (temp != null){
-temp.next = newtemp.next;
-temp = temp.next;
-if(temp != null){
-    newtemp.next = temp.next;
-    newtemp = newtemp.next;
-}
-      }
-return newHead;
-
-    }
-}
+     }
+        temp = head;
+        while(temp!=null){
+            if(temp.random != null){
+            temp.next.random = temp.random.next;
+            }
+            temp = temp.next.next;
+        }
+            temp = head;
+            Node newHead = head.next;
+            Node newtemp = newHead;
+            while(temp!= null){
+              temp.next = newtemp.next;
+              temp = temp.next;
+              if(temp != null){  
+                  newtemp.next = temp.next;
+                  newtemp = newtemp.next;
+            }
+            }
+            return newHead;
+        }
+     }
+    
