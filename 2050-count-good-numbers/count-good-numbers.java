2265fc5d93515fd74730688cf3ea4959e9988e73
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2025-09-08 02:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (98.19%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -1,24 +1,26 @@
-class Solution {
-    static final int MOD = 1_000_000_007;
-
-    // Fast power function (binary exponentiation)
-    public long pow(long base, long exp) {
-        long result = 1;
-        while (exp > 0) {
-            if ((exp & 1) == 1) {   // if exp is odd
-                result = (result * base) % MOD;
-            }
-            base = (base * base) % MOD; // square the base
-            exp >>= 1; // divide exp by 2
+class Solution{
+    private long MOD = 1_000_000_007;
+    private long pow(long base, long exp){
+        long result =1;
+        while(exp>0){
+        if((exp & 1)==1){
+            
+            result = (result*base)%MOD;
+        }
+        base = (base*base)%MOD;
+        exp>>=1;
         }
         return result;
     }
 
-    // Function to count good numbers of length n
-    public int countGoodNumbers(long n) {
-        long evenCount = (n + 1) / 2; // even index positions (0, 2, 4, ...)
-        long oddCount = n / 2;        // odd index positions (1, 3, 5, ...)
-        long res = (pow(5, evenCount) * pow(4, oddCount)) % MOD;
-        return (int) res;
-    }
+
+public int countGoodNumbers(long n){
+    long even = (n+1)/2;
+    long odd = n/2;
+    long res =(pow(5,even)*pow(4,odd))%MOD;
+    return (int)res;
+    
+            }
 }
+   
+       
