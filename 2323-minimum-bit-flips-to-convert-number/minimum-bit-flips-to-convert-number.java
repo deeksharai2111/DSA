dc0ff8c4bb46092e13185a3bfe45959ecc963aad
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2025-11-27 00:24:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (26.17%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -3,6 +3,16 @@ class Solution {
         int xorValue = start ^ goal;
         
         // Count how many 1s are present → number of flips needed
-        return Integer.bitCount(xorValue);
+        int count = 0;
+
+        // check all 32 bits of an integer
+        for (int i = 0; i < 32; i++) {
+            // mask to check ith bit
+            if ((xorValue & (1 << i)) != 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
