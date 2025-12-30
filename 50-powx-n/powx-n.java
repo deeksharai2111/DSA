cfa60d2d17a730df13652e787e3554effd5c3ea2
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2025-12-31 01:03:20 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (53.68%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,20 @@
-class Solution{
-public double myPow(double x, int n){
-double ans = 1.0;
-long nn = n;
-if(nn < 0) nn = -1 *nn;
-while(nn>0){
-    if(nn%2==1){
-        ans = ans * x;
-        nn = nn-1;
-    }else{
-        x = x*x;
-        nn=nn/2;
+class Solution {
+    public double myPow(double x, int n) {
+     long power = n;
+     double ans = 1.0;
+     if(power < 0){
+        x=1/x;
+        power = -power;
+     } 
+      while(power >0){
+        if (power %2 ==1){
+            ans *= x;
+            power--;
+        }else{
+            x *=x;
+            power /= 2;
+        }
+        }
+        return ans;
+      } 
     }
-}
-    if(n<0)
-     ans = (double)(1.0)/(double)(ans);
-    
-return ans;
-    }
-}
