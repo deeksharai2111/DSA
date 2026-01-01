f98d1d40417b953244b7e448be694db024d4ffb2
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2026-01-02 00:59:22 +0530

    Time: 1 ms (9.11%) | Memory: 47.7 MB (78.41%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution{
     public double myPow(double x, int n) {
      long power = n;
      double ans = 1.0;
@@ -6,7 +6,7 @@ class Solution {
         x=1/x;
         power = -power;
      } 
-      while(power >0){
+      while(power > 0){
         if (power %2 ==1){
             ans *= x;
             power--;
