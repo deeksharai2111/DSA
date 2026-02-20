ffaef1e333f52d8ee82678882b41e70c0d0ba157
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2026-02-21 00:42:18 +0530

    Time: 89 ms (12.48%) | Memory: 47.5 MB (92.02%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,23 +1,29 @@
-import java.util.*;
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        
         int n = nums.length;
         int[] nge = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && st.peek() <= nums[i % n]) {
-                st.pop();
-            }
-            if (i < n) {
-                if (st.isEmpty()) {
-                    nge[i] = -1;
-                } else {
-                    nge[i] = st.peek();
+        
+        // Initialize all values with -1
+        for (int i = 0; i < n; i++) {
+            nge[i] = -1;
+        }
+        
+        // Outer loop
+        for (int i = 0; i < n; i++) {
+            
+            // Check next n-1 elements circularly
+            for (int j = i + 1; j <= i + n - 1; j++) {
+                
+                int ind = j % n;  // circular index
+                
+                if (nums[ind] > nums[i]) {
+                    nge[i] = nums[ind];
+                    break;
                 }
             }
-            st.push(nums[i % n]);
         }
-
+        
         return nge;
     }
 }
