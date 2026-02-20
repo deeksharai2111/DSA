e1ab1b8f111fcd16d5a91af25885ee8526dc3228
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2026-02-21 00:40:51 +0530

    Time: 15 ms (54.13%) | Memory: 48.6 MB (56.32%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,24 +1,20 @@
 import java.util.*;
-
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        
         int n = nums.length;
         int[] nge = new int[n];
-        Arrays.fill(nge, -1);
-
-        Deque<Integer> st = new ArrayDeque<>();
-
+        Stack<Integer> st = new Stack<>();
         for (int i = 2 * n - 1; i >= 0; i--) {
-
             while (!st.isEmpty() && st.peek() <= nums[i % n]) {
                 st.pop();
             }
-
-            if (i < n && !st.isEmpty()) {
-                nge[i] = st.peek();
+            if (i < n) {
+                if (st.isEmpty()) {
+                    nge[i] = -1;
+                } else {
+                    nge[i] = st.peek();
+                }
             }
-
             st.push(nums[i % n]);
         }
 
