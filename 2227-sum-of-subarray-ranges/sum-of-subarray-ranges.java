27855f3f6b3496efdf1314cac7a307dacd2f677a
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2026-04-10 16:28:18 +0530

    Time: 26 ms (41.23%) | Memory: 47.7 MB (7.57%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,62 +1,54 @@
 class Solution{
     public long subArrayRanges(int[] nums) {
-        int n = nums.length;
-        long minSum = 0, maxSum = 0;
-        Stack<Integer> st = new Stack<>();
-        // ---------- MINIMUM ----------
-        int[] pse = new int[n];
-        int[] nse = new int[n];
-        // PSE (Previous Smaller Element)
-        
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && nums[st.peek()] > nums[i]) {
-                st.pop();
-            }
-            pse[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
+     int n=nums.length;
+     long minSum = 0,maxSum = 0;
+     Stack<Integer> st = new Stack<>();
+     int[] nse = new int[n];
+     int[] pse = new int[n];
+     st.clear();
+     for(int i=0;i<n;i++){
+        while(!st.isEmpty() && nums[st.peek()] > nums[i]){
+            st.pop();
         }
-        // NSE (Next Smaller Element)
-        st.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && nums[st.peek()] >= nums[i]) {
-                st.pop();
-            }
-            nse[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
+        pse[i] = st.isEmpty() ? -1:st.peek();
+        st.push(i);
+     }
+     st.clear();
+     for(int i=n-1;i>=0;i--){
+        while(!st.isEmpty() && nums[st.peek()] >= nums[i]){
+            st.pop();
         }
-       // Calculate min contribution
-        for (int i = 0; i < n; i++) {
-            long left = i - pse[i];
-            long right = nse[i] - i;
-            minSum += left * right * nums[i];
+        nse[i] = st.isEmpty()?n:st.peek();
+        st.push(i);
+     }
+     for(int i =0;i<n;i++){
+        long left = i - pse[i];
+        long right = nse[i]-i;
+        minSum += left*right*nums[i];
+     }
+     int[] nge = new int[n];
+     int[] pge = new int[n];
+     st.clear();
+     for(int i = 0;i<n;i++){
+        while(!st.isEmpty() && nums[st.peek()] < nums[i]){
+            st.pop();
         }
-        // ---------- MAXIMUM ----------
-        int[] pge = new int[n];
-        int[] nge = new int[n];
-        // PGE (Previous Greater Element)
-        st.clear();
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && nums[st.peek()] < nums[i]) {
-                st.pop();
-            }
-            pge[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
+       pge[i] = st.isEmpty()?-1:st.peek();
+       st.push(i);
+     }
+     st.clear();
+     for(int i = n-1;i>=0;i--){
+        while(!st.isEmpty() && nums[st.peek()] <= nums[i]){
+            st.pop();
         }
-        // NGE (Next Greater Element)
-        st.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) {
-                st.pop();
-            }
-            nge[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
-        // Calculate max contribution
-        for (int i = 0; i < n; i++) {
-            long left = i - pge[i];
-            long right = nge[i] - i;
-            maxSum += left * right * nums[i];
-        }
-         return maxSum - minSum;
+        nge[i] = st.isEmpty()?n:st.peek();
+        st.push(i);
+     }
+     for(int i = 0;i<n;i++){
+        long left = i-pge[i];
+        long right = nge[i]-i;
+        maxSum +=left*right*nums[i];
+     }
+     return maxSum-minSum;
     }
 }
