dd6c0c1f89cd94f7657bd5b5aedd959c50e5ae8e
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2026-05-02 18:07:18 +0530

    Time: 34 ms (21.07%) | Memory: 54.3 MB (59.76%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,18 +1,14 @@
 class StockSpanner {
-    Stack<int[]> stack;
+    Stack<int[]> stack = new Stack<>();  // declare + create in one line
 
-    public StockSpanner() {
-        stack = new Stack<>();
-    }
-
+    // constructor is now REMOVED because stack is already created above
+    
     public int next(int price) {
         int span = 1;
-
         while (!stack.isEmpty() && stack.peek()[0] <= price) {
             span += stack.peek()[1];
             stack.pop();
         }
-
         stack.push(new int[]{price, span});
         return span;
     }
