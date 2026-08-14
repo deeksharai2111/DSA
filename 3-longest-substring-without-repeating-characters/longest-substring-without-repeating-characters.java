c8ac7c030f77a4f335b2af079093f59d886bd841
Deeksha Rai <106366545+deeksharai2111@users.noreply.github.com>
2026-08-15 02:21:27 +0530

    Time: 11 ms (34.31%) | Memory: 47.2 MB (30.64%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,46 +1,24 @@
-class Solution {
+class Solution{
     public int lengthOfLongestSubstring(String s) {
-
         int n = s.length();
-
-        // hash[i] stores the last index where character i was seen
         int[] hash = new int[256];
-
-        // Initially, no character has been seen
-        for (int i = 0; i < 256; i++) {
+        for(int i=0; i<256; i++){
             hash[i] = -1;
         }
-
-        int l = 0;       // Left pointer
-        int r = 0;       // Right pointer
-        int maxLen = 0;  // Maximum length found
-
-        while (r < n) {
-
-            // If current character was seen before
-            if (hash[s.charAt(r)] != -1) {
-
-                // If previous occurrence is inside current window
-                if (hash[s.charAt(r)] >= l) {
-
-                    // Move left pointer after previous occurrence
-                    l = hash[s.charAt(r)] + 1;
-                }
-            }
-
-            // Calculate current window length
-            int len = r - l + 1;
-
-            // Update maximum length
-            maxLen = Math.max(maxLen, len);
-
-            // Store the current index of the character
-            hash[s.charAt(r)] = r;
-
-            // Move right pointer
-            r++;
+        int l = 0;
+        int r = 0;
+        int maxlen = 0;
+        while(r < n){
+      if (hash[s.charAt(r)] != -1){
+      if  (hash[s.charAt(r)] >= l){
+       l =  hash[s.charAt(r)] + 1;
+      }
+      }
+      int len = r-l+1;
+     maxlen = Math.max(maxlen, len);
+        hash[s.charAt(r)] = r;
+        r++;
         }
-
-        return maxLen;
-    }
+        return maxlen;
+   }
 }
